7fbf381c8b94bc1283a10a6f39478860c9e0d853
Petrus Pierre <petrusfla@gmail.com>
2020-07-12 13:33:11 -0300

    Bug de usuario sem foto corrigido

--- src/commands/image/morrepraga.js
+++ src/commands/image/morrepraga.js
@@ -6,22 +6,38 @@ module.exports = {
   aliases: ['morre', 'praga'],
   description: 'Cria uma imagem morre praga com o avatar ou o nome da pessoa ou imagem da internet',
   usage: '<@usuario> ou <nome> ou <link para imagem>',
-  cooldown: 10,
+  cooldown: 5,
   args: true,
   async execute(_, message, args) {
-    const bg = await fetch('https://pbs.twimg.com/media/EWI1_WkWkAEcgul.jpg');
+    try {
+      const bg = await fetch('https://pbs.twimg.com/media/EWI1_WkWkAEcgul.jpg');
 
-    let target = message.mentions.members.first();
+      let target = message.mentions.members.first();
 
-    if (!target) {
-      target = args[0];
-      message.channel.send('Carregando...');
+      if (!target) {
+        target = args[0];
+        message.channel.send('Carregando...');
 
-      if (target.includes('https://') || target.includes('http://')) {
-        const avatar = await fetch(args[0]);
+        if (target.includes('https://') || target.includes('http://')) {
+          const avatar = await fetch(args[0]);
+          const img = new Canvas(1080, 649)
+            .addImage(await bg.buffer(), 0, 0, 1080, 649)
+            .addCircularImage(await avatar.buffer(), 270, 330, 120)
+            .toBuffer();
+
+          message.channel.bulkDelete(2);
+          message.channel.send({ files: [img] }).catch((err) => {
+            console.log(err);
+            message.reply('não consegui fazer esta imagem');
+          });
+
+          return;
+        }
         const img = new Canvas(1080, 649)
           .addImage(await bg.buffer(), 0, 0, 1080, 649)
-          .addCircularImage(await avatar.buffer(), 270, 330, 120)
+          .setColor('#000000')
+          .setTextSize(46)
+          .addText(target, 200, 335, 150)
           .toBuffer();
 
         message.channel.bulkDelete(2);
@@ -32,37 +48,28 @@ module.exports = {
 
         return;
       }
+
+      const avatarURL = target.user.avatarURL({ format: 'jpg' });
+
+      const avatar = await fetch(avatarURL || 'https://discordapp.com/assets/322c936a8c8be1b803cd94861bdfa868.png');
+
+      message.channel.send('Carregando...');
+
       const img = new Canvas(1080, 649)
         .addImage(await bg.buffer(), 0, 0, 1080, 649)
-        .setColor('#000000')
-        .setTextSize(46)
-        .addText(target, 200, 335, 150)
+        .addCircularImage(await avatar.buffer(), 280, 330, 100)
+        .setTextSize(30)
+        .addText(target.user.username, 230, 460, 120)
         .toBuffer();
 
       message.channel.bulkDelete(2);
+
       message.channel.send({ files: [img] }).catch((err) => {
         console.log(err);
         message.reply('não consegui fazer esta imagem');
       });
-
-      return;
+    } catch (err) {
+      console.error(err);
     }
-    const avatar = await fetch(target.user.avatarURL({ format: 'jpg' }));
-
-    message.channel.send('Carregando...');
-
-    const img = new Canvas(1080, 649)
-      .addImage(await bg.buffer(), 0, 0, 1080, 649)
-      .addCircularImage(await avatar.buffer(), 280, 330, 100)
-      .setTextSize(30)
-      .addText(target.user.username, 230, 460, 120)
-      .toBuffer();
-
-    message.channel.bulkDelete(2);
-
-    message.channel.send({ files: [img] }).catch((err) => {
-      console.log(err);
-      message.reply('não consegui fazer esta imagem');
-    });
   },
 };
