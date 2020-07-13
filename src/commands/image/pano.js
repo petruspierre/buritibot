29d3bebc04bf31e104e950c8e505a51fccc8371d
Petrus Pierre <petrusfla@gmail.com>
2020-07-12 23:23:49 -0300

    Adicionado opcao de mencao no +pano

--- src/commands/image/pano.js
+++ src/commands/image/pano.js
@@ -4,21 +4,51 @@ const fetch = require('node-fetch');
 module.exports = {
   name: 'pano',
   aliases: ['aiai'],
-  description: 'Cria uma imagem o chão é... com a imagem de alguém',
-  usage: '<@usuario> <chão>',
+  description: 'Passa pano para alguém',
+  usage: '<alguem | algo> ou <@usuario> <alguem | algo>',
   cooldown: 5,
   args: true,
   async execute(_, message, args) {
     const bg = await fetch('https://i.pinimg.com/736x/bd/1b/8a/bd1b8a5cf03ed3a414fe19b0312010a9.jpg');
 
+    const target = message.mentions.members.first();
+
+    if (!target) {
+      const img = new Canvas(720, 521)
+        .addImage(await bg.buffer(), 0, 0, 720, 521)
+        .setTextSize(34)
+        .setColor('#fff')
+        .setTextAlign('center')
+        .addResponsiveText(`aiai esse ${args.join(' ')}`, 530, 300, 260)
+        .toBuffer();
+
+      message.channel.bulkDelete(1);
+
+      message.channel.send({ files: [img] }).catch((err) => {
+        console.log(err);
+        message.reply('não consegui fazer esta imagem');
+      });
+
+      return;
+    }
+
+    const avatarURL = target.user.avatarURL({ format: 'jpg' });
+
+    const avatar = await fetch(avatarURL || 'https://discordapp.com/assets/322c936a8c8be1b803cd94861bdfa868.png');
+
     message.channel.send('Carregando...');
+
+    const title = args.slice(1);
+
+    const avatarBuffer = await avatar.buffer();
 
     const img = new Canvas(720, 521)
       .addImage(await bg.buffer(), 0, 0, 720, 521)
+      .addCircularImage(avatarBuffer, 320, 150, 80)
       .setTextSize(34)
       .setColor('#fff')
       .setTextAlign('center')
-      .addResponsiveText(`aiai esse ${args.join(' ')}`, 530, 300, 260)
+      .addResponsiveText(`aiai esse ${title.join(' ')}`, 530, 300, 260)
       .toBuffer();
 
     message.channel.bulkDelete(1);
