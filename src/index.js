6940b0c6baec2595f6475b1831524c74ef00f4c3
Petrus Pierre <petrusfla@gmail.com>
2020-08-04 21:06:06 -0300

    Adicionado suporte ao Telegram

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const fs = require('fs');
 const { resolve } = require('path');
 const Discord = require('discord.js');
+const TelegramBot = require('node-telegram-bot-api');
 const YouTube = require('youtube-node');
 
 const { prefix, defaultCooldown } = require('../config.json');
 
+const bot = new TelegramBot(process.env.TELEGRAM_KEY, { polling: true });
 const client = new Discord.Client();
 const cooldowns = new Discord.Collection();
 client.commands = new Discord.Collection();
@@ -105,6 +107,40 @@ client.on('message', async (message) => {
   } catch (error) {
     console.error(error);
     message.reply('ocorreu um erro ao tentar executar esse comando');
+  }
+});
+
+bot.on('message', async (msg) => {
+  const content = msg.text.toString().toLowerCase();
+  if (!content.startsWith(prefix)) return;
+
+  const args = content.slice(prefix.length).split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  const command = client.commands.get(commandName)
+    || client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+
+  if (!command) return bot.sendMessage(msg.chat.id, 'Não entendi, tem certeza que esse comando existe?');
+
+  if (!command.telegram) return bot.sendMessage(msg.chat.id, 'Desculpe, esse comando não está disponível para o Telegram');
+
+  if (command.disabled) return bot.sendMessage(msg.chat.id, 'Desculpe, esse comando está desativado');
+
+  if (command.args && !args.length) {
+    let reply = 'Preciso de mais informações para executar esse comando!';
+
+    if (command.usage) {
+      reply += `\nA maneira correta é: \`${prefix}${command.name} ${command.usage}\``;
+    }
+
+    return bot.sendMessage(msg.chat.id, reply);
+  }
+
+  try {
+    command.execute_telegram(bot, msg, args);
+  } catch (error) {
+    console.error(error);
+    bot.sendMessage(msg.chat.id, 'ocorreu um erro ao tentar executar esse comando');
   }
 });
 
