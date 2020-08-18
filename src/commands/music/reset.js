47d65a03182d30e2ecbb7a03dc9076ed807f5119
Petrus Pierre <petrusfla@gmail.com>
2020-08-18 01:33:46 -0300

    Reset fix

--- src/commands/music/reset.js
+++ src/commands/music/reset.js
@@ -14,8 +14,7 @@ module.exports = {
       );
     }
 
-    serverQueue.songs = [];
-    serverQueue.connection.dispatcher.end();
+    serverQueue = {};
     return message.channel.send('Resetando!');
   },
 };
