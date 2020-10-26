b33b91d11aa45feae019c58d4b8ca70618394d98
Petrus Pierre <petrusfla@gmail.com>
2020-10-25 23:39:55 -0300

    Novas mensagens para o telegram

--- src/commands/fun/buriti.js
+++ src/commands/fun/buriti.js
@@ -39,14 +39,34 @@ module.exports = {
     message.reply(respostas[index]);
   },
   async execute_telegram(bot, msg, args) {
-    const result = Math.floor(Math.random() * 2);
-    const index = Math.floor(Math.random() * 4);
+    const index = Math.floor(Math.random() * 22);
 
-    const listaNao = ['Não.', 'Definitivamente não.', 'Não acredito que seja verdade.', 'Não é verdade.'];
-    const listaSim = ['Sim.', 'Definitivamente sim.', 'Sim, é verdade.', 'Todo mundo sabe que sim.'];
+    const respostas = [
+      'Não.',
+      'Definitivamente não.',
+      'Não acredito que seja verdade.',
+      'Não é verdade.',
+      'Sim.',
+      'Definitivamente sim.',
+      'Sim, é verdade.',
+      'Todo mundo sabe que sim.',
+      'Não sei e nem quero saber.',
+      'Siga seu coração.',
+      '***.',
+      'Por favor me deixa em paz.',
+      'Parece que sim.',
+      'Acho que não.',
+      'Pode ser que sim, mas também pode ser que não.',
+      'Não posso revelar a verdade sobre isso.',
+      'Talvez não.',
+      'De jeito nenhum.',
+      'Claro.',
+      'Talvez sim.',
+      'Me pergunta daqui 5 minutos.',
+      'Não sei e não quero saber.',
+      'Se você acredita que sim, essa é a resposta correta.',
+    ];
 
-    result === 1
-      ? bot.sendMessage(msg.chat.id, listaSim[index], { reply_to_message_id: msg.message_id })
-      : bot.sendMessage(msg.chat.id, listaNao[index], { reply_to_message_id: msg.message_id });
+    bot.sendMessage(msg.chat.id, respostas[index], { reply_to_message_id: msg.message_id });
   },
 };
