097d0bf1968cffaee9da774eb1651a96171aa1ff
Petrus Pierre <petrus.pierre@codeminer42.com>
2020-12-26 11:00:38 -0300

    New events folder

---
+++ src/events/discord/listenDiscordCommands.js
@@ -0,0 +1,50 @@
+import Discord from 'discord.js';
+import YouTube from 'youtube-node';
+
+import validateDiscordMessage from '../../validators/validateDiscordMessage';
+import { prefix, serverID } from '../../../config.json';
+
+const youtube = new YouTube();
+youtube.setKey(process.env.YOUTUBE_KEY);
+
+const queue = new Map();
+const bingo = new Map();
+const cooldowns = new Discord.Collection();
+
+function listenDiscordCommands(client) {
+  client.on('message', async (message) => {
+    if (!validateDiscordMessage(message, client, cooldowns)) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command = client.commands.get(commandName)
+      || client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+
+    try {
+      if (command.category === 'Música') {
+        const channel = message.guild.channels.cache.find((ch) => ch.name === '🎶bot-de-musica');
+        if (!channel) return message.reply('Por favor crie o canal `🎶bot-de-musica`');
+        if (message.channel.name !== '🎶bot-de-musica') return message.reply(`Por favor utilize o canal ${channel}`);
+
+        const serverQueue = queue.get(message.guild.id);
+        command.execute(client, message, args, serverQueue, queue, youtube);
+      } else if (command.name === 'bingo') {
+        let serverBingo;
+        if (message.channel.type !== 'dm') {
+          serverBingo = bingo.get(message.guild.id);
+        } else {
+          serverBingo = bingo.get(serverID);
+        }
+        command.execute(client, message, args, serverBingo, bingo);
+      } else {
+        command.execute(client, message, args);
+      }
+    } catch (error) {
+      console.error(error);
+      message.reply('ocorreu um erro ao tentar executar esse comando');
+    }
+  });
+}
+
+export default listenDiscordCommands;
--- src/events/index.js
+++ src/events/index.js
@@ -1,9 +1,21 @@
-import guildMemberAdd from './guildMemberAdd';
-import guildMemberRemove from './guildMemberRemove';
+import guildMemberAdd from './discord/guildMemberAdd';
+import guildMemberRemove from './discord/guildMemberRemove';
+import listenDiscordCommands from './discord/listenDiscordCommands';
+import listenTelegramCommands from './telegram/listenTelegramCommands';
 
-function listenEvents(client) {
-  guildMemberRemove(client);
-  guildMemberAdd(client);
+function listenDiscordEvents(discord) {
+  guildMemberRemove(discord);
+  guildMemberAdd(discord);
+  listenDiscordCommands(discord);
+}
+
+function listenTelegramEvents(telegram, discord) {
+  listenTelegramCommands(telegram, discord);
+}
+
+function listenEvents(discord, telegram) {
+  listenDiscordEvents(discord);
+  listenTelegramEvents(telegram, discord);
 }
 
 export default listenEvents;
