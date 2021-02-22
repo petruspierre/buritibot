f49a656ab079f1b2862c39bbb18aee58d4b6d507
Petrus Pierre <petrus.pierre@codeminer42.com>
2021-02-22 17:11:42 -0300

    Disable music commmands

--- src/events/index.js
+++ src/events/index.js
@@ -1,20 +1,22 @@
 import guildMemberAdd from './discord/guildMemberAdd';
+import guildCreate from './discord/guildCreate';
 import guildMemberRemove from './discord/guildMemberRemove';
 import listenDiscordCommands from './discord/listenDiscordCommands';
 import listenTelegramCommands from './telegram/listenTelegramCommands';
 
-function listenDiscordEvents(discord) {
+function listenDiscordEvents(discord, youtube) {
   guildMemberRemove(discord);
   guildMemberAdd(discord);
-  listenDiscordCommands(discord);
+  guildCreate(discord);
+  listenDiscordCommands(discord, youtube);
 }
 
 function listenTelegramEvents(telegram, discord) {
   listenTelegramCommands(telegram, discord);
 }
 
-function listenEvents(discord, telegram) {
-  listenDiscordEvents(discord);
+function listenEvents(discord, telegram, youtube) {
+  listenDiscordEvents(discord, youtube);
   listenTelegramEvents(telegram, discord);
 }
 
