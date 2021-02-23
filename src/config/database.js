e0eddbde370230bb28e47555c520ecd6d94a53bb
Petrus Pierre <petrus.pierre@academico.ifpb.edu.br>
2021-02-23 16:25:47 -0300

    Updated database.js

--- src/config/database.js
+++ src/config/database.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   production: {
     dialect: 'postgres',
-    host: process.env.DATABASE_URL,
+    host: process.env.DATABASE_HOST,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_TABLE,
