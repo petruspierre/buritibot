bdbcd9afcb0d88c9b4d2b6b0d0592c74f467374e
Petrus Pierre <petrus.pierre@codeminer42.com>
2021-02-23 15:39:08 -0300

    [WIP] Guild controller

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,26 @@
 module.exports = {
-  dialect: 'postgres',
-  host: process.env.DATABASE_HOST,
-  username: 'postgres',
-  password: 'docker',
-  database: 'buriti',
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
+  development: {
+    dialect: 'postgres',
+    host: process.env.DATABASE_URL,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_TABLE,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  },
+  production: {
+    dialect: 'postgres',
+    host: process.env.DATABASE_URL,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_TABLE,
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
   },
 };
