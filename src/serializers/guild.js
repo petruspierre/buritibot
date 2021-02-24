f348cba5c85f039faa5effe2c8e7b42a0ea503d4
Petrus Pierre <petrus.pierre@codeminer42.com>
2021-02-24 15:28:36 -0300

    Guild data camel case serializer

---
+++ src/serializers/guild.js
@@ -0,0 +1,13 @@
+function guildCamelCase(guild) {
+  return {
+    prefix: guild.prefix,
+    wellcomeMessage: guild.wellcome_message,
+    wellcomeChannel: guild.wellcome_channel,
+    musicChannel: guild.music_channel,
+    botChannel: guild.bot_channel,
+    alertCommandDontExist: guild.alert_command_dont_exist,
+    defaultCooldown: guild.default_cooldown,
+  };
+}
+
+export { guildCamelCase };
