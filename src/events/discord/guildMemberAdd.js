344497eb8c3d2eff97490f50ddde4c460491e17c
Petrus Pierre <petrus.pierre@codeminer42.com>
2021-06-18 18:21:15 -0300

    Fix joining guild error

--- src/events/discord/guildMemberAdd.js
+++ src/events/discord/guildMemberAdd.js
@@ -1,11 +1,11 @@
 function guildMemberAdd(client) {
   client.on('guildMemberAdd', (member) => {
     const channel = member.guild.channels.cache.find((ch) => ch.name === '🚪entradas-e-saidas');
+    if (!channel) return;
     member.setNickname(`${(member.displayName).toUpperCase()} WTFF`);
 
     const cargoPrincipal = member.guild.roles.cache.find((role) => role.name === 'wonkru');
     member.roles.add(cargoPrincipal);
-    if (!channel) return;
 
     channel.send(`Bem vindo ao ${member.guild.name}, ${member}\nBeba bastante água 🥛`);
   });
