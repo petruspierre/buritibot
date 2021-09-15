c7ece978dae72d9cf117b4da519af32b7ee5f184
Petrus Pierre <petrus.tsx@gmail.com>
2021-09-15 00:04:33 -0300

    Update play.js

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -11,7 +11,7 @@ module.exports = {
   guildOnly: true,
   args: true,
   cooldown: 3,
-  disabled: true,
+  disabled: false,
   async execute(client, message, args, serverQueue, queue, youtube) {
     const { channel } = message;
     const voiceChannel = message.member.voice.channel;
